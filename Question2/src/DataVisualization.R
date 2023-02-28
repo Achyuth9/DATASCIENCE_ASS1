59e0289ba24fe8be1f38cb0047530556ae49ded7
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-02-27 23:03:41 -0600

    Update DataVisualization.R

--- Question2/src/DataVisualization.R
+++ Question2/src/DataVisualization.R
@@ -1,22 +1,3 @@
-### Sunflower plot between reading score amd math score
-sunflowerplot(MYFILE$reading.score, MYFILE$math.score,
-              +               rotate = TRUE,
-              +               main = "Plot of carb vs. gear")
-
-
-###barplot between the genders of the data
-x <- sample(c("female","male"), 100, replace=TRUE)
-barplot(table(x),col=c("pink","red"))
-
-
-###box plot between the scores of the data
-boxplot(MYFILE$math.score,MYFILE$reading.score,MYFILE$writing.score, main="BOX PLOT FOR THE SCORES", col=c("red","blue","green"))
-
-###histogram data between the reading score
-hist(MYFILE$reading.score, main="Histogram of reading score",col='blue')
-
-###qq norm 
-qqnorm(MYFILE$math.score)
 
 ###READING CLEAN STUDENTS PERFORMANCE FILE AND VISUALIZING THE DATA
 
@@ -40,5 +21,11 @@ hist(spfile$math.score, main="Histogram of Students math score",col='green',ylim
 ###barplot
 myvar <- sample(c(spfile$parental.level.of.education), 100, replace=TRUE)
 barplot(table(myvar),col=c("magenta","red","orange","darkgreen","blue","brown"),ylim=c(0,35),main="Barplot of Parent level of ethinicity")
+
+
+###boxplot
+boxplot(spfile$math.score,spfile$reading.score,spfile$writing.score, main="BOX PLOT FOR THE SCORES", col=c("green","yellow","red"))
+
+
 
 
