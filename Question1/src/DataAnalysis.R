51badc0f5c62db8d23c99bd41e77b21852cd0aa7
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-02-27 22:00:12 -0600

    Update DataAnalysis.R

--- Question1/src/DataAnalysis.R
+++ Question1/src/DataAnalysis.R
@@ -1,4 +1,4 @@
 clean_people_data <- read.csv("~/Desktop/ACHYUTHAYINALA/Question1/clean_data/CLEANDATA.csv")
-t_test_result <- with(clean_people_data, t.test(Age~Frailty))
-t_test_result
-capture.output(t_test_result, file="~/Desktop/ACHYUTHAYINALA/Question1/results/Test_Output.txt")
+My_t_test_result <- with(clean_people_data, t.test(Age~Frailty))
+MY_t_test_result
+capture.output(MY_t_test_result, file="~/Desktop/ACHYUTHAYINALA/Question1/results/Test_Output.txt")
