890cf79b2c2e77b78edd76dc3240fd5db031b6f9
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-02-27 23:07:39 -0600

    Update DataAnalysis.R

--- Question1/src/DataAnalysis.R
+++ Question1/src/DataAnalysis.R
@@ -1,4 +1,12 @@
+
+###Loading the cleaned data for analysis
 clean_people_data <- read.csv("~/Desktop/ACHYUTHAYINALA/Question1/clean_data/CLEAN_DATA.csv")
+
+###performing the t_test on Age and Frailty
 My_t_test_result <- with(clean_people_data, t.test(Age~Frailty))
+
+###printing the output of the test on console
 MY_t_test_result
+
+###Storing the result into a Test_Output.txt file
 capture.output(MY_t_test_result, file="~/Desktop/ACHYUTHAYINALA/Question1/results/Test_Output.txt")
