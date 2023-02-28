12991d7c78a8a145db4ff6796d388bdeb968b8f4
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-02-27 23:08:41 -0600

    Update DataAnalysis.R

--- Question1/src/DataAnalysis.R
+++ Question1/src/DataAnalysis.R
@@ -1,4 +1,3 @@
-
 ###Loading the cleaned data for analysis
 clean_people_data <- read.csv("~/Desktop/ACHYUTHAYINALA/Question1/clean_data/CLEAN_DATA.csv")
 
@@ -10,3 +9,4 @@ MY_t_test_result
 
 ###Storing the result into a Test_Output.txt file
 capture.output(MY_t_test_result, file="~/Desktop/ACHYUTHAYINALA/Question1/results/Test_Output.txt")
+
