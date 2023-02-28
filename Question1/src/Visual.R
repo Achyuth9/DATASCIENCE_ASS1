f635237ec6c245507e4ac27126c5a5ad3987294c
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-02-27 22:19:01 -0600

    Create Visual.R

---
+++ Question1/src/Visual.R
@@ -0,0 +1,2 @@
+cleaned_data <- read.csv("~/Desktop/ACHYUTHAYINALA/Question1/clean_data/CLEAN_DATA.csv")
+plot(cleaned_data$Age, cleaned_data$Grip_Strength,xlab="Age",ylab="Grip Strength",main="Age vs Grip Strength", col="blue")
